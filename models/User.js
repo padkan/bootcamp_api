4cab7a16b8837449264c309019a3f65d7bfc1589
saeed padkan <s.padkan@gmail.com>
2023-12-08 13:23:15 +0100

    add auth and register a user endpoint

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    require: [true, "Please add an email"],
+    unique: true,
+    match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Please add a valid email"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    Required: [true, "Please add a Password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+// Encrypt password using bcrypt
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+module.exports = mongoose.model("User", UserSchema);
